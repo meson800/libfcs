8e523726261d1ebca14cbf95d4aba020eb8837e0
Christopher Johnstone <meson800@gmail.com>
2022-03-20 00:22:43 -0400

    Updated to handle most of the FCS metadata commands

--- src/FCS.hs
+++ src/FCS.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use bimap" #-}
 module FCS
     ( readFCS
     ) where
 
+import Data.Time as Time
 import qualified Data.ByteString.Lazy as BL
 import Data.Int ( Int32, Int64 )
 import Data.Binary.Get (Get, getByteString, skip, lookAhead, Decoder(Fail), runGet)
@@ -12,6 +15,7 @@ import Text.Read (readMaybe)
 import Data.Void (Void)
 import Control.Monad (liftM, liftM2, liftM3)
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 import qualified Data.Text as T
 import qualified Data.Text.Encoding
 import Data.Char (isAscii)
@@ -149,6 +153,11 @@ data ParameterVisualizationScale = ParameterVisualizationScale
     , f2 :: Float
     } deriving (Show)
 
+data ParameterCalibration = ParameterCalibration
+    { unitConversionFactor :: Float
+    , unitName :: T.Text
+    } deriving (Show)
+
 data Parameter = Parameter
     { bitLength :: Int64  -- PnB
     , amplification :: AmplificationType -- PnE
@@ -164,11 +173,12 @@ data Parameter = Parameter
     , name :: Maybe T.Text -- PnS
     , detectorType :: Maybe T.Text -- PnT
     , detectorVoltage :: Maybe Float -- PnV
+    , calibration :: Maybe ParameterCalibration -- PnCALIBRATION
     } deriving (Show)
 
 
-getParameter :: Int64 -> Map.Map T.Text T.Text -> Get Parameter
-getParameter i keyvalues = Parameter
+getParameter :: Map.Map T.Text T.Text -> Int64 -> Get Parameter
+getParameter keyvalues i = Parameter
                     <$> parseKeyval (T.pack $ "$P" ++ show i ++ "B") keyvalues
                     <*> ((\[decades, offset] -> liftM2 AmplificationType
                             (maybeGet "Unable to parse amplification decades type" (readMaybeText decades))
@@ -189,21 +199,151 @@ getParameter i keyvalues = Parameter
                     <*> return (Map.lookup (T.pack $ "$P" ++ show i ++ "S") keyvalues)
                     <*> return (Map.lookup (T.pack $ "$P" ++ show i ++ "T") keyvalues)
                     <*> maybeParseKeyval (T.pack $ "$P" ++ show i ++ "F") keyvalues
+                    <*> (if Map.notMember (T.pack $ "$P" ++ show i ++ "CALIBRATION") keyvalues then return Nothing else (\[cFactor, name] -> Just <$> liftM2 ParameterCalibration
+                            (maybeGet "Unable to parse parameter calibration unit conversion" (readMaybeText cFactor))
+                            (return name)
+                        ) =<< parseTokens 2 (T.singleton  ',') (T.pack $ "$P" ++ show i ++ "CALIBRATION") keyvalues)
+
+data FCSMode = List | MultivariateHistogram | UnivariateHistograms deriving (Enum, Show)
+toFCSMode :: T.Text -> Get FCSMode
+toFCSMode text
+    | c == Just (T.singleton 'L') = return List
+    | c == Just (T.singleton 'C') = return MultivariateHistogram
+    | c == Just (T.singleton 'U') = return UnivariateHistograms
+    | otherwise = fail "Unable to parse FCS mode"
+    where c = if T.length text == 1 then Just text else Nothing
+
+data ByteOrder = LittleEndian | BigEndian deriving (Enum, Show)
+toByteOrder :: T.Text -> Get ByteOrder
+toByteOrder text
+    | text == T.pack "1,2,3,4" = return LittleEndian
+    | text == T.pack "4,3,2,1" = return BigEndian
+    | otherwise = fail "Unable to parse byte order"
+
+data Originality = Original | NonDataModified | Appended | DataModified deriving (Enum, Show)
+toOriginality :: T.Text -> Get Originality
+toOriginality text
+    | text == T.pack "Original" = return Original
+    | text == T.pack "NonDataModified" = return NonDataModified
+    | text == T.pack "Appended" = return Appended
+    | text == T.pack "DataModified" = return DataModified
+    | otherwise = fail "Unable to parse originality"
+
+data Trigger = Trigger
+    { triggerChannel :: T.Text
+    , triggerValue :: Int64
+    } deriving (Show)
+
+data CellSubset = CellSubset
+    { numSimultaneousSubsets :: Int64 -- CSMODE
+    , numSubsets :: Maybe Int64 -- CSTOT
+    , subsetNBits :: Maybe Int64 -- CSVBITS
+    , flagMap :: Map.Map Int64 Int64 -- CSVnFLAG
+    } deriving (Show)
+
+maybeCellSubset :: Map.Map T.Text T.Text -> Get (Maybe CellSubset)
+maybeCellSubset keyvals = case Map.lookup (T.pack "$CSMODE") keyvals of
+        Nothing -> return Nothing
+        Just csmode -> do
+            numSimultaneousSubsets <- maybeGet "Unable to parse $CSMODE" $ readMaybeText csmode
+            numSubsets <- maybeParseKeyval (T.pack "$CSTOT") keyvals
+            subsetNBits <- maybeParseKeyval (T.pack "$CSVBITS") keyvals
+            let intersectKeyvals = Map.toList $ Map.restrictKeys keyvals $ Set.fromList [T.pack $ "$CSV" ++ show i ++ "FLAG" | i <- [1..numSimultaneousSubsets]]
+            flagMap <- Map.fromList <$> mapM (maybeGet2 . \(a,b) -> (readMaybeText $ T.drop 4 $ T.dropEnd 4 a, readMaybeText b)) intersectKeyvals
+            return (Just $ CellSubset numSimultaneousSubsets numSubsets subsetNBits flagMap)
+
+
 
 data FCSMetadata = FCSMetadata
-    { mode :: T.Text -- MODE
-    , byteOrder :: T.Text -- BYTEORD
+    { mode :: FCSMode -- MODE
+    , byteOrder :: ByteOrder -- BYTEORD
     , nParameters :: Int64 -- PAR
-    , nEvents :: Int64 -- TOT
     , parameters :: [Parameter]
+    , nEvents :: Int64 -- TOT
     , extraKeyvals :: Map.Map T.Text T.Text
     -- Optional parameters
-    , nEventsLost :: Maybe Int64 -- ABRT
-    , acquireTime :: Maybe T.Text -- BTIM
+    , nEventsAborted :: Maybe Int64 -- ABRT
+    , acquireTime :: Maybe Time.LocalTime -- BTIM
+    , acquireDate :: Maybe Time.Day -- DATE
+    , acquireEndTime :: Maybe Time.LocalTime -- ETIM
+    , cellSubset :: Maybe CellSubset -- CSMODE, CSTOT, CSVBITS, CSVnFLAG
     , cells :: Maybe T.Text -- CELLS
     , comment :: Maybe T.Text -- COM
-    , cellSubsetMode :: Maybe T.Text
+    , cytometerType :: Maybe T.Text -- CYT
+    , cytometerSerialNumber :: Maybe T.Text -- CYTSN
+    , institution :: Maybe T.Text -- INST
+    , experimenter :: Maybe T.Text -- EXP
+    , operator :: Maybe T.Text -- OP
+    , filename :: Maybe T.Text -- FIL
+    , lastModified :: Maybe Time.LocalTime -- LAST_MODIFIED
+    , lastModifier :: Maybe T.Text -- LAST_MODIFIER
+    , nEventsLost :: Maybe Int64 -- LOST
+    , originality :: Maybe Originality -- ORIGINALITY
+    , plateID :: Maybe T.Text -- PLATEID
+    , plateName :: Maybe T.Text -- PLATENAME
+    , project :: Maybe T.Text -- PROJ
+    , specimen :: Maybe T.Text -- SMNO
+--    , spillover :: Maybe T.Text -- SPILLOVER
+    , specimenSource :: Maybe T.Text -- SRC
+    , computer :: Maybe T.Text -- SYS
+    , timestep :: Maybe Float -- TIMESTEP
+    , trigger :: Maybe Trigger -- TR
+    , volume :: Maybe Float -- VOL
+    , wellID :: Maybe T.Text -- WELLID
     } deriving (Show)
+
+
+getMetadata :: Map.Map T.Text T.Text -> Get FCSMetadata
+getMetadata keyvalues = do
+    mode <- toFCSMode =<< maybeGet "Missing $MODE parameter" (Map.lookup (T.pack "$MODE") keyvalues)
+    byteOrder <- toByteOrder =<< maybeGet "Missing $BYTEORD parameter" (Map.lookup (T.pack "$BYTEORD") keyvalues)
+    nParameters <- parseKeyval (T.pack "$PAR") keyvalues
+    parameters <- mapM (getParameter keyvalues) [1..nParameters]
+    nEvents <- parseKeyval (T.pack "$TOT") keyvalues
+    let extraKeyvals = Map.filter ((\case
+                                        Just val -> fst val /= '$'
+                                        Nothing -> False) . T.uncons) keyvalues
+    nEventsAborted <- maybeParseKeyval (T.pack "$ABRT") keyvalues
+    acquireStartTime <- maybeGetTime False Time.defaultTimeLocale  "%H:%M:%S%Q" $ Map.lookup (T.pack "$BTIM") keyvalues
+    acquireDate <- maybeGetTime False Time.defaultTimeLocale "%d-%b-%0Y" $ Map.lookup (T.pack "$DATE") keyvalues
+    acquireEndTime <- maybeGetTime False Time.defaultTimeLocale  "%H:%M:%S%Q" $ Map.lookup (T.pack "$ETIM") keyvalues
+    cellSubset <- maybeCellSubset keyvalues
+    let cells = Map.lookup (T.pack "$CELLS") keyvalues
+    let comment = Map.lookup (T.pack "$COM") keyvalues
+    let cytometerType = Map.lookup (T.pack "$CYT") keyvalues
+    let cytometerSN = Map.lookup (T.pack "$CYTSN") keyvalues
+    let institution = Map.lookup (T.pack "$INST") keyvalues
+    let experimenter = Map.lookup (T.pack "$EXP") keyvalues
+    let operator = Map.lookup (T.pack "$OP") keyvalues
+    let filename = Map.lookup (T.pack "$FIL") keyvalues
+    lastModified <- maybeGetTime False Time.defaultTimeLocale  "%d-%b-%0Y %H:%M:%S%Q" $ Map.lookup (T.pack "$LAST_MODIFIED") keyvalues
+    let lastModifier = Map.lookup (T.pack "$LAST_MODIFIER") keyvalues
+    nEventsLost <- maybeParseKeyval (T.pack "$LOST") keyvalues
+    originality <- (\case
+                        Nothing -> return Nothing
+                        Just val -> Just <$> toOriginality val) $ Map.lookup (T.pack "$ORIGINALITY") keyvalues
+    let plateID = Map.lookup (T.pack "$PLATEID") keyvalues
+    let plateName = Map.lookup (T.pack "$PLATENAME") keyvalues
+    let project = Map.lookup (T.pack "$PROJ") keyvalues
+    let specimen = Map.lookup (T.pack "$SMNO") keyvalues
+
+    let specimenSource = Map.lookup (T.pack "$SRC") keyvalues
+    let computer = Map.lookup (T.pack "$SYS") keyvalues
+    timestep <- maybeParseKeyval (T.pack "$TIMESTEP") keyvalues
+    trigger <- (if Map.notMember (T.pack "$TR") keyvalues then return Nothing else (\[tCName, tValue] -> Just <$> fmap (Trigger tCName)
+                            (maybeGet "Unable to parse trigger channel value" (readMaybeText tValue))
+                        ) =<< parseTokens 2 (T.singleton  ',') (T.pack "$TR") keyvalues)
+    volume <- maybeParseKeyval (T.pack "$VOL") keyvalues
+    let wellID = Map.lookup (T.pack "$WELLID") keyvalues
+
+    return $! FCSMetadata
+                mode byteOrder nParameters parameters nEvents
+                extraKeyvals nEventsAborted acquireStartTime
+                acquireDate acquireEndTime cellSubset cells comment
+                cytometerType cytometerSN institution experimenter
+                operator filename lastModified lastModifier nEventsLost
+                originality plateID plateName project specimen
+                specimenSource computer timestep trigger volume wellID
 
 data FCS = FCS
     { header :: Header
@@ -241,10 +381,22 @@ maybeGet errMsg m = case m of
     Just a -> return a
     Nothing -> fail errMsg
 
+maybeGet2 :: (Maybe a, Maybe b) -> Get (a,b)
+maybeGet2 text = case text of
+    (Just a, Just b) -> return (a,b)
+    _ -> fail "Unable to parse tuple"
+
 parseKeyval :: Read a => T.Text -> Map.Map T.Text T.Text -> Get a
 parseKeyval key map = case Map.lookup key map of
                 Nothing -> fail $ T.unpack key ++ " keyval pair missing!"
                 Just val -> maybeGet (T.unpack key ++ " value failed to parse.") (readMaybeText val)
+
+-- Fails if parsing fails, but returns Nothing if given Nothing
+maybeGetTime :: Time.ParseTime t => Bool -> Time.TimeLocale -> String -> Maybe T.Text -> Get (Maybe t)
+maybeGetTime acceptWhitespace locale format input = case input of
+    Just val -> Just <$> maybeGet "Failed to parse datetime" (Time.parseTimeM acceptWhitespace locale format (T.unpack val))
+    Nothing -> return Nothing
+
 
 maybeParseKeyval :: Read a => T.Text -> Map.Map T.Text T.Text -> Get (Maybe a)
 maybeParseKeyval key map = case Map.lookup key map of
